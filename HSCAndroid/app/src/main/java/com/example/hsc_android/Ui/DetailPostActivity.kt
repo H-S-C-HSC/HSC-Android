be54004fd6f385ce7f6b6be2e39a614c565d7d7c
minhee <jwmh15@gmail.com>
2020-11-29 23:27:23 +0900

    Add detailPostActivity

---
+++ HSCAndroid/app/src/main/java/com/example/hsc_android/Ui/DetailPostActivity.kt
@@ -0,0 +1,12 @@
+package com.example.hsc_android.network.Ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.hsc_android.R
+
+class DetailPostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_post)
+    }
+}
